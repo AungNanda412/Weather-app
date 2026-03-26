e31e3b605ac8fc084d9d2a843375ef95412f2656
AungNanda412 <ahpaung41@gmail.com>
2026-03-26 21:33:27 +0630

    Feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+import axios from 'axios'
+
+function App() {
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=51eecac8cdafc754c007cdb06c0be92f`
+
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  )
+}
+
+export default App
